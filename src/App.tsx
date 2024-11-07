b12717cf5dcb47053d33327f4737c665eef41cfd
amirabas <amirjabic@gmail.com>
2024-11-07 18:32:45 +0330

    create exam for selected id

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import { forwardRef, useImperativeHandle, useRef,useState } from "react";
+import React, {
+    forwardRef,
+    MutableRefObject,
+    useImperativeHandle,
+    useRef,
+    useState,
+} from "react";
+
+interface TypeChildCompotnt {
+    data: {
+        id: number;
+        title: string;
+    }[];
+    selectedId: number | null;
+    handleSelected: (id: number) => void;
+}
 
 export default function App() {
-    const ref = useRef();
+    const [selectedId, setSelectedId] = useState<number | null>(null);
+
+    function handleSelected(id: number) {
+        if (id) {
+            setSelectedId(id);
+        }
+    }
+
+    const data = [
+        {
+            id: 1,
+            title: "react",
+        },
+        {
+            id: 2,
+            title: "javascirpt",
+        },
+        {
+            id: 3,
+            title: "vue js",
+        },
+    ];
 
     return (
         <div>
-            <ChildComponent ref={ref} />
-            <span className="cursor-pointer px-3 py-1 bg-red-300" onClick={()=> ref.current.clickOn()}>
-                is click
-            </span>
+            <ChildComponent
+                data={data}
+                selectedId={selectedId}
+                handleSelected={handleSelected}
+            />
+            {selectedId}
         </div>
     );
 }
 
-const ChildComponent = forwardRef((props, ref) => {
-    const buttonRef = useRef();
-    const [isClick,setIsCLick] = useState(false)
-
-    useImperativeHandle(ref, () => ({
-        clickOn() {
-            setIsCLick((prev)=> {
-                return !prev
-            })
-        },
-    }));
+const ChildComponent: React.FC<TypeChildCompotnt> = (props) => {
+    const { data, handleSelected } = props;
 
     return (
-        <div>
-            <button ref={buttonRef} className="px-3 py-1 rounded-md bg-blue-200" type="text" onClick={()=> setIsCLick(!isClick)}>
-                    {isClick && "isCLick"}
-            </button>
+        <div className='flex gap-2'>
+            {data.map((i) => (
+                <button
+                    className={`px-2 py-1 bg-blue-300 rounded-md`}
+                    key={i.id}
+                    onClick={() => handleSelected(i.id)}>
+                    {i.title}
+                </button>
+            ))}
         </div>
     );
-});
+};
